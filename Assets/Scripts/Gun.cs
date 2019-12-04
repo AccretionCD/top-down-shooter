b86a0083313a000cea2ebe6391de042ec59c3111
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-12-04 11:49:12 +0000

    Added reloading system

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,7 +12,11 @@ public class Gun : MonoBehaviour
 
     [SerializeField] float rateOfFire = 10f;
     [SerializeField] float fireVelocity = 10f;
+    [SerializeField] int bulletsPerMagazine = 50;
+    [SerializeField] float reloadSpeed = 5f;
     float timeTillShot;
+    int bulletsRemaining;
+    bool reloading;
 
     MuzzleFlash muzzleFlash;
 
@@ -31,6 +35,13 @@ public class Gun : MonoBehaviour
     void Start()
     {
         shotsPerBurstRemaining = shotsPerBurst;
+        bulletsRemaining = bulletsPerMagazine;
+    }
+
+    void Update()
+    {
+        if (!reloading && bulletsRemaining <= 0 || Input.GetKeyDown(KeyCode.R))
+            StartCoroutine(Reload());
     }
 
     public void OnTriggerPress()
@@ -47,7 +58,7 @@ public class Gun : MonoBehaviour
 
     void Fire()
     {
-        if (Time.time > timeTillShot)
+        if (Time.time > timeTillShot && bulletsRemaining > 0 && !reloading)
         {
             if (fireMode == FireMode.Burst)
             {
@@ -63,18 +74,46 @@ public class Gun : MonoBehaviour
                     return;
             }
 
-            for (int i = 0; i < bullets.Length; i++)
+            if (bulletsRemaining != 0)
             {
-                float randomBulletSpread = Random.Range(-bulletSpread, bulletSpread);
-                Quaternion randomRotation = Quaternion.Euler(randomBulletSpread, randomBulletSpread, randomBulletSpread);
+                for (int i = 0; i < bullets.Length; i++)
+                {
+                    float randomBulletSpread = Random.Range(-bulletSpread, bulletSpread);
+                    Quaternion randomRotation = Quaternion.Euler(randomBulletSpread, randomBulletSpread, randomBulletSpread);
 
-                timeTillShot = Time.time + 100 / rateOfFire;
-                Bullet newBullet = Instantiate(bullets[i], muzzle.position, muzzle.rotation * randomRotation);
-                newBullet.SetSpeed(fireVelocity);
+                    timeTillShot = Time.time + 100 / rateOfFire;
+                    Bullet newBullet = Instantiate(bullets[i], muzzle.position, muzzle.rotation * randomRotation);
+                    newBullet.SetSpeed(fireVelocity);
+                    bulletsRemaining--;
+                }
+
+                Instantiate(shell, extractor.position, extractor.rotation * Quaternion.Euler(0, 180, 0));
+                muzzleFlash.Activate();
             }
-
-            Instantiate(shell, extractor.position, extractor.rotation * Quaternion.Euler(0, 180, 0));
-            muzzleFlash.Activate();
         }
+    }
+
+    IEnumerator Reload()
+    {
+        reloading = true;
+
+        yield return new WaitForSeconds(0.25f);
+
+        float percent = 0;
+        Vector3 rotation = transform.localEulerAngles;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * reloadSpeed;
+
+            float interpolation = 4 * (-Mathf.Pow(percent, 2) + percent);
+            float reloadAngle = Mathf.Lerp(0, 25, interpolation);
+            transform.localEulerAngles = rotation + Vector3.left * reloadAngle;
+
+            yield return null;
+        }
+
+        reloading = false;
+        bulletsRemaining = bulletsPerMagazine;
     }
 }
