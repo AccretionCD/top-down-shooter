9b1e21d0a850491996198901d9e30f6fe920f03b
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-12-14 09:49:11 +0000

    Added basic main menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,32 +7,73 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] Image fadeScreen;
     [SerializeField] GameObject gameOverScreen;
+    [SerializeField] Image waveBanner;
+    [SerializeField] Text waveText;
 
     PlayerController player;
+    SpawnHandler spawnHandler;
+
+    void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+        spawnHandler = FindObjectOfType<SpawnHandler>();
+    }
 
     void Start()
     {
-        player = FindObjectOfType<PlayerController>();
         player.OnDeath += OnPlayerDeath;
+        spawnHandler.OnWaveStart += OnWaveStart;
+    }
+
+    void OnWaveStart(int waveNumber)
+    {
+        waveText.text = "Wave " + waveNumber;
+
+        StartCoroutine(FadeWaveBanner());
     }
 
     void OnPlayerDeath()
     {
-        StartCoroutine(FadeScreen(Color.clear, Color.black, 1));
+        StartCoroutine(FadeScreen());
         gameOverScreen.SetActive(true);
+        Cursor.visible = true;
     }
 
-
-
-    IEnumerator FadeScreen(Color original, Color target, float duration)
+    IEnumerator FadeWaveBanner()
     {
-        float rate = 1 / duration;
         float percent = 0;
 
         while (percent < 1)
         {
-            percent += Time.deltaTime * rate;
-            fadeScreen.color = Color.Lerp(original, target, percent);
+            percent += Time.deltaTime * 2;
+            waveBanner.color = Color.Lerp(Color.clear, Color.black, percent);
+            waveText.color = Color.Lerp(Color.clear, Color.white, percent);
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 2;
+            waveBanner.color = Color.Lerp(Color.black, Color.clear, percent);
+            waveText.color = Color.Lerp(Color.white, Color.clear, percent);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeScreen()
+    {
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime;
+            fadeScreen.color = Color.Lerp(Color.clear, Color.black, percent);
 
             yield return null;
         }
@@ -40,6 +81,7 @@ public class UIManager : MonoBehaviour
 
     public void PlayAgain()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+        Cursor.visible = false;
     }
 }
